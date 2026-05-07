cfb90a13282d305c288fc0587e3892c857ddca75
Rajaram Yadav <yrajaram112@gmail.com>
2026-05-07 12:36:27 -0500

    favicon update

--- components/providers.tsx
+++ components/providers.tsx
@@ -6,7 +6,7 @@ import { LangProvider } from "@/lib/i18n";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <LangProvider>
         {children}
         <Toaster position="bottom-center" />
